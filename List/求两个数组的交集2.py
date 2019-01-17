eb8d3f155dfde375bdfcdbb60c040874f7932dc2
Perfect-You <37954594+Perfect-You@users.noreply.github.com>
2019-01-17 18:07:31 +0800

    Update 求两个数组的交集2.py

--- List/求两个数组的交集2.py
+++ List/求两个数组的交集2.py
@@ -1,1 +1,19 @@
-
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        a=[]
+        dict={}
+        for num in nums1:
+            if num not in dict:
+                dict[num]=1
+            else:
+                dict[num]+=1
+        for num in nums2:
+            if num in dict and dict[num]>0:
+                a.append(num)
+                dict[num]-=1
+        return a
