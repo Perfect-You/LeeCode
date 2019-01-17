c0b098619196d462b60c219b8e8e15ed7a5ea14a
Perfect-You <37954594+Perfect-You@users.noreply.github.com>
2019-01-17 18:15:35 +0800

    Update 加一.py

--- List/加一.py
+++ List/加一.py
@@ -1,1 +1,18 @@
-
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        a=digits
+        a[-1]+=1
+        for n in range(len(a)):
+            for i in range(len(a)):
+                if a[i]==10:
+                    if i!=0:
+                        a[i]=0
+                        a[i-1]+=1
+                    if i==0:
+                        a[i]=0
+                        a.insert(0,1)
+        return a
