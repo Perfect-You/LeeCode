9c45c750acff0cc3ac6a297ebacfc738dd3406ca
Perfect-You <37954594+Perfect-You@users.noreply.github.com>
2019-03-11 18:04:59 +0800

    Create Shuffle an Array.py

---
+++ Design questions/Shuffle an Array.py
@@ -0,0 +1,35 @@
+class Solution(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.nums=nums[:]
+        self.new=nums
+
+    def reset(self):
+        """
+        Resets the array to its original configuration and return it.
+        :rtype: List[int]
+        """
+        return self.nums
+        
+
+    def shuffle(self):
+        """
+        Returns a random shuffling of the array.
+        :rtype: List[int]
+        """
+        n=len(self.new)
+        for i in range(n):
+            j=random.randint(i,n-1)
+            self.new[i],self.new[j]=self.new[j],self.new[i]
+        return self.new
+        
+    
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
